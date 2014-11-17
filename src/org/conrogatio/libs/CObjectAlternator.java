7bbf28b394a9b4a7a0c51771fafcf2b23d7d7a42
Jonas Enlund <enlund.jonas@gmail.com>
2014-11-17 14:41:09 +0100

    added CObjectAlternator

---
+++ src/org/conrogatio/libs/CObjectAlternator.java
@@ -0,0 +1,46 @@
+package org.conrogatio.libs;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CObjectAlternator {
+	private static Random rand = new Random();
+	private static ArrayList<Object> o;
+	private int i = -1;
+	
+	private static int randInt() {
+		return rand.nextInt(o.size());
+	}
+	
+	public CObjectAlternator(ArrayList<Object> objects) {
+		o = objects;
+	}
+	
+	public Object getNextObject() {
+		i++;
+		if (i == o.size()) {
+			i = 0;
+		}
+		return o.get(i);
+	}
+	
+	public Object getRandomObject() {
+		return o.get(randInt());
+	}
+	
+	public void changeObjects(ArrayList<Object> objects) {
+		o = objects;
+	}
+	
+	public void replaceObject(Object object, int position) {
+		o.set(position, object);
+	}
+	
+	public void addObject(Object object) {
+		o.add(object);
+	}
+	
+	public ArrayList<Object> getObjects() {
+		return o;
+	}
+}
